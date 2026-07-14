78973d02739939f343d34c43d3e06dc179f6f5f8
Nithya <nithyavelmurugan.v@gmail.com>
2026-07-14 12:57:41 +0530

    save

---
+++ Frontendproject/myapp/views.py
@@ -0,0 +1,16 @@
+# from django.shortcuts import render
+
+# # Create your views here.
+
+# from django.shortcuts import render
+# from django.http import HttpResponse
+
+# def members(request):
+#     return HttpResponse("Hello world!")
+
+from django.http import HttpResponse
+from django.template import loader
+
+def members(request):
+  template = loader.get_template('index.html')
+  return HttpResponse(template.render())
